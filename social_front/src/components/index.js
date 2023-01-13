5ebed8a37853e6efa6a27bcedecc89f6b63f54f6
Glen112 <escobar.glen@gmail.com>
2023-01-13 13:01:15 -0300

    navbar

--- social_front/src/components/index.js
+++ social_front/src/components/index.js
@@ -1,8 +1,11 @@
-export { default as Sidebar } from './sidebar';
-export { default as UserProfile } from './userProfile';
-// export { default as Sidebar } from './sidebar'
-// export { default as Sidebar } from './sidebar'
-// export { default as Sidebar } from './sidebar'
-// export { default as Sidebar } from './sidebar'
-// export { default as Sidebar } from './sidebar'
-
+export { default as CreatePin } from '../components/createPin/index';
+export { default as Feed } from '../components/feed/index';
+export { default as Login } from '../components/login/index';
+export { default as Navbar } from '../components/navBar/index';
+export { default as Sidebar } from '../components/sidebar/index';
+export { default as UserProfile } from '../components/userProfile/index';
+export { default as Search } from '../components/search/index';
+export { default as PinDetail } from '../components/pinDetail/index';
+// export { default as Sidebar } from './search'
+// export { default as Sidebar } from './search'
+// export { default as Sidebar } from './search'
--- social_front/src/containers/home/index.js
+++ social_front/src/containers/home/index.js
@@ -13,8 +13,8 @@ import logo from '../../assets/logo.png'
 const Home = () => {
   const [toggleSidebar, setToggleSidebar] = useState(false);
   const [ user, setUser ] = useState(null)
-  const scrollRef = useRef(null);
   const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+  const scrollRef = useRef(null);
 
   useEffect(()=>{
     const query = userQuery(userInfo?.googleId)
@@ -29,6 +29,8 @@ const Home = () => {
   },[])
 
   // console.log(userInfo)
+  //overflow-y-auto z-10
+  //w-full flex
   return(
     <div className="flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out ">
       <div className="hidden md:flex h-screen flex-initial">
@@ -43,18 +45,15 @@ const Home = () => {
         <Link to={`user-profile/${user?._id}`}>
           <img src={user?.image} alt="logo" width={80} className="w-28"/>
         </Link>
-      {toggleSidebar && (
-      <div class="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
-        <div className="absolute w-full flex  justify-end items-center p-2">
-          <AiFillCloseCircle fontSize={30} className="cursor-pointer" onClick={()=> setToggleSidebar(false)} />
+        </div>
+        {toggleSidebar && (
+      <div class="fixed w-4/5 bg-white shadow-md h-screen overflow-y-auto z-10 animate-slide-in">
+        <div className="absolute w-full flex justify-end items-center p-2">
+          <AiFillCloseCircle fontSize={25} className="cursor-pointer z-1" onClick={()=> setToggleSidebar(false)} />
         </div>
         <Sidebar user={user && user} closeToggle={setToggleSidebar} />
       </div>
       )}
-
-        </div>
-      </div>
-      <div className="flex md:hidden flex-row">
       </div>
       <div className="pb-2 flex-1 h-screen overflow-y-scroll" ref={scrollRef}>
         <Routes>
