7bb1412a047111f9c8df5c80e100564b02e704b6
Glen112 <escobar.glen@gmail.com>
2023-02-27 23:27:50 -0300

    user

--- social_front/src/components/galeria/index.js
+++ social_front/src/components/galeria/index.js
@@ -13,54 +13,54 @@ const Galeria = ({user}) => {
   const [nCarta, setNCarta] = useState(0);
   const [audio, setAudio] = useState('');
   const [error, setError] = useState("");
-  const access = user.access
-  const navigate = useNavigate()
-console.log(access);
-  useEffect(() => {
-    setLoading(true)
-    const query = cartasMoreQuery(nCarta)
+      // const access = user.access
+      const navigate = useNavigate()
+    // console.log(access);
+      useEffect(() => {
+        setLoading(true)
+        const query = cartasMoreQuery(nCarta)
 
-    client.fetch(query)
-      .then((data) => {
-        setCartas(data)
-        const myAudio = data[0].audio.asset.url
-        setAudio(myAudio)
-        setLoading(false)
-      })
-      .catch(e =>{
-        setError(e)
-      })
-    
-},[nCarta, error]);
+        client.fetch(query)
+          .then((data) => {
+            setCartas(data)
+            const myAudio = data[0].audio.asset.url
+            setAudio(myAudio)
+            setLoading(false)
+          })
+          .catch(e =>{
+            setError(e)
+          })
+        
+    },[nCarta, error]);
 
-  useEffect(() => {
-   if(access == false) return navigate('/shop', { replace:true } )
-    
-  },[access, navigate]);
-  return(
-    <div className=" flex flex-col item-center justify-center p-5">
-      <div className="flex items-center ">
-        <h1 className="flex mx-auto mb-6">Galeria</h1>
-      </div>
-      <div className="max-w-40% border-solid border-2 border-white rounded-xl pb-6 mx-auto flex flex-col">
-      {
-        cartas ? cartas.map((carta) => <Carta className="p-12 p-12" pin={carta} key={carta} audio={carta.audio} />) : <Spinner message='Loading'/>
-      }
-      
-      <div className="flex flex-col items-center justify-center">
-      
+      useEffect(() => {
+       if(access == false) return navigate('/shop', { replace:true } )
+        
+      },[access, navigate]);
+      return(
+        <div className=" flex flex-col item-center justify-center p-5">
+          <div className="flex items-center ">
+            <h1 className="flex mx-auto mb-6">Galeria</h1>
+          </div>
+          <div className="max-w-40% border-solid border-2 border-white rounded-xl pb-6 mx-auto flex flex-col">
+          {
+            cartas ? cartas.map((carta) => <Carta className="p-12 p-12" pin={carta} key={carta} audio={carta.audio} />) : <Spinner message='Loading'/>
+          }
+          
+          <div className="flex flex-col items-center justify-center">
+          
 
-      </div>
-      </div>
-       <div className="flex flex-col mx-auto mt-8">
-         <Audio audio={audio}/>
-      
-       </div>
-      <div>
-        <Pagination nCarta={nCarta} setNCarta={setNCarta} />
-      </div>
-    </div>
-    )
-}
+          </div>
+          </div>
+           <div className="flex flex-col mx-auto mt-8">
+             <Audio audio={audio}/>
+          
+           </div>
+          <div>
+            <Pagination nCarta={nCarta} setNCarta={setNCarta} />
+          </div>
+        </div>
+        )
+    }
 
-export default Galeria
+    export default Galeria
